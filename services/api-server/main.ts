d8d65a7a3fd2cf8c6c8dac39e9f32f2471015b5f
Eyal Rotbart <6060053+EyalRo@users.noreply.github.com>
2025-10-03 20:16:34 -0700

    scaffolding for users api calls

--- services/api-server/main.ts
+++ services/api-server/main.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import users from "./users.ts"
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello, World!"));
+app.route('/users', users)
 
 Deno.serve(app.fetch);
